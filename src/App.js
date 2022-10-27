0bcd688c9309198e4a0bb4d8322df4b807d44d12
jajablinky <gmillar6@gmail.com>
2022-10-27 11:10:06 -0700

    basic routing set up and components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes, Navigate } from "react-router-dom";
+import { Key } from 'react';
+import Photos from './components/Photos';
+import Nav from './components/Nav';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container">
+      <Nav />
+      <Routes>
+        <Route path='/photos' element={<Photos />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
